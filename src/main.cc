e60cf2cbe39ae681c861ee39b62e19c22f6458d1
Prober76 <ugartegrover@gmail.com>
2022-12-01 16:29:48 -0500

    Ejecucion lista

--- src/main.cc
+++ src/main.cc
@@ -4,21 +4,5 @@ int main()
 {
     Interfaz aplicacion;
     aplicacion.ejecutar();
-    // Arbol_R_Hilbert arbol;
-    // arbol.insertar({{169, 566}});
-    // arbol.insertar({{431, 570}});
-    // arbol.insertar({{686, 452}});
-    // arbol.insertar({{879, 318}});
-    // arbol.insertar({{145, 390}});
-    // arbol.insertar({{678, 191}});
-    // arbol.insertar({{936, 130}});
-    // arbol.insertar({{172, 694}});
-    // arbol.insertar({{733, 679}});
-    // arbol.insertar({{921, 258}});
-    // arbol.insertar({{188, 169}});
-    // arbol.insertar({{555, 473}});
-    // arbol.eliminar({529, 491});
-    // arbol.eliminar({536, 270});
-    // arbol.eliminar({382, 332});
     return 0;
 }
